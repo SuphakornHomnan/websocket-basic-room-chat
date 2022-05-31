90fa1f5a5c17a9399210d0dcf94a6621712d8a63
Suphanice <supakorn.h@wdepic.com>
2022-05-31 14:25:07 +0700

    intial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppGateway } from './app.gateway';
+import { ChatGateway } from './chat/chat.gateway';
+import { AlertGateway } from './alert/alert.gateway';
+import { AlertController } from './alert/alert.controller';
+
+@Module({
+  imports: [],
+  controllers: [AlertController],
+  providers: [AppGateway, ChatGateway, AlertGateway],
+})
+export class AppModule {}
